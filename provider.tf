da7e4df3900a4fc78fdda182dd1bfbe4e2f5d649
derekbelrose <derek@derekbelrose.com>
2018-07-16 10:56:17 -0400

    Configured a remote state bucket in s3

--- provider.tf
+++ provider.tf
@@ -2,3 +2,12 @@ provider "aws" {
     region = "us-east-1"
     profile = "jhuadmin"
 }
+
+terraform {
+    backend "s3" {
+        profile = "jhuadmin"
+        bucket = "msel-terraform-states"
+        region = "us-east-1"
+        key = "applicatons/msel-jeff-aws-sandbox/state"
+    }
+}
